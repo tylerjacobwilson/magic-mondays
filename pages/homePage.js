cc769edaf3a2b9325c62816ba1f4da450b4d625b
tylerjacobwilson <83513320+tylerjacobwilson@users.noreply.github.com>
2023-05-30 17:50:18 -0700

    Added hover over

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,60 +1,66 @@
-import React from "react"
-import Image from "next/image"
-import Link from "next/link"
-import Minotaur from "../public/Images/Minotaur_05.png"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Minotaur from "../public/Images/Minotaur_05.png";
 
 export default function HomePage() {
   return (
-    <div className='w-screen h-screen bg-black text-white flex'>
-      <div className='flex flex-col justify-between'>
+    <div className="w-screen h-screen bg-black text-white flex">
+      <div className="flex flex-col justify-between">
         <div>
-          <h1 className='text-lg w-72 leading-5 mb-10'>
-            Welcome to Magic Mondays! The best place on the web to manage your play group.
-          </h1>
-          <div className='bg-black text-white'>
-            <Link href='/contact'>
-              <div>Contact</div>
-            </Link>
-            <Link href='/user'>
-              <div>User</div>
-            </Link>
-            <nav>
-              <ul>
-                <li>
-                  <a href='#'>Home</a>
-                </li>
-                <li>
-                  <a href='#'>About</a>
-                </li>
-                <li>
-                  <a href='#'>Contact</a>
-                </li>
-              </ul>
-            </nav>
-          </div>
           <br />
-          <h1>Sign In</h1>
-          <form action='#' method='post'>
-            <label htmlFor='username'>Username:</label>
-            <input type='text' id='username' name='username' />
+
+          <h1
+            className="text-lime-300 text-3xl margin: 20px w-screen leading-5 mb-10"
+            style={{
+              textShadow:
+                "-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000",
+            }}
+          >
+            Welcome to Magic Mondays! The best place to manage your play group.
+          </h1>
+
+          <h1
+            className="text-blue-500 text-3xl margin: 20px w-screen leading-5 mb-10"
+            style={{
+              textShadow:
+                "-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000",
+            }}
+          >
+            Sign In:
+          </h1>
+
+          <form action="#" method="post">
+            <label htmlFor="username">Username: </label>
+            <input type="text" id="username" name="username" />
             <br />
             <br />
-            <label htmlFor='password'>Password:</label>
-            <input type='password' id='password' name='password' />
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" name="password" />
             <br />
             <br />
-            <input type='submit' value='Sign In' />
+
             <p>
-              Do not have an account? <a href='signUp'>Create one</a>
+              Don't have an account?{" "}
+              <a
+                className="text-red-600 text-3xl margin: 20px w-screen leading-5 mb-10"
+                style={{
+                  textShadow:
+                    "-2px -2px 0 #000, 2px -2px 0 #000, -2px 2px 0 #000, 2px 2px 0 #000",
+                }}
+                href="signUp"
+              >
+                Create one
+              </a>
             </p>
           </form>
         </div>
       </div>
-      <div className='mx-auto'>
+      <div className="mx-auto">
         <div>
           <Image src={Minotaur} height={500} />
         </div>
       </div>
     </div>
-  )
+  );
 }
